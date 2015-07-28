2951d2eefa7b3d3733d8f7786828b402ba57c789
hendri@techedemic.com <hendri@techedemic.com>
2015-07-28 11:32:00 +0200

    Added ntpclient.cs, Util.cs and created ntp based functions in Program.cs

--- rpmkit/Program.cs
+++ rpmkit/Program.cs
@@ -1,15 +1,121 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Configuration;
 
 namespace rpmkit
 {
     class Program
     {
+        public static int g_AttrNum = 0;
+
         static void Main(string[] args)
         {
+            try
+            {
+
+
+                if (args.Length == 0)
+                {
+                    string procName = Process.GetCurrentProcess().ProcessName;
+                    Console.WriteLine("No arguments were provided");
+                    Console.WriteLine("Usage: ");
+                    Console.WriteLine(procName + " [attribute number] [functionName] [function parameter 1] [function parameter 2] ... ");
+                    Console.WriteLine("Example:");
+                    Console.WriteLine(procName + " 12345 randomNumber 1 100");
+                    Console.WriteLine(" *** This will generate a random number between 1 and 100 and write it to 14504.dat");
+                    return;
+                }
+
+                int attributeNumber = int.Parse(args[0]);
+                Program.g_AttrNum = attributeNumber;
+                string rpmFunction = args[1];
+                Util.writeLog("main", "rpmFunction : " + rpmFunction, false);
+
+                switch (rpmFunction)
+                {
+                    case "randomNumber":
+                        func_randomNumber(attributeNumber, int.Parse(args[2]), int.Parse(args[3]));
+                        break;
+                    case "compareTime":
+                        func_compareTime(attributeNumber, args[2]);
+                        break;
+                    default:
+                        Console.WriteLine("No 'functioname' provided or unable to locate function requested");
+                        break;
+                }
+            }
+            catch (Exception exc)
+            {
+                Util.writeLog("main", "EXCEPTION : " + exc.Message.ToString(), false);
+                Console.WriteLine(exc.Message.ToString());
+            }
+        }
+
+        /// <summary>
+        /// The func_compareTime() function will take two arguments
+        /// attributeNumber (int) - The value of the attribute number which should be passed through by the main method
+        /// ntpServer (string) - Either an IP Address or Hostname of an NTP server which is accessible from the host where the script is running
+        /// </summary>
+
+        static void func_compareTime(int attributeNumber, string ntpServer)
+        {
+            try
+            {
+                // Get NTP server time                
+                var ntpTime = ntpclient.GetNetworkTime(ntpServer).ToUniversalTime();
+
+                // Get the local time of the host
+                var localTime = DateTime.UtcNow;
+                Util.writeLog("func_compareTime", "ntpTime:   " + ntpTime.Hour + ":" + ntpTime.Minute + ":" + ntpTime.Minute + ":" + ntpTime.Second + ":" + ntpTime.Millisecond, true);
+                Util.writeLog("func_compareTime", "localTime: " + localTime.Hour + ":" + localTime.Minute + ":" + localTime.Minute + ":" + localTime.Second + ":" + localTime.Millisecond, true);
+
+                // Calculate the difference between the two times
+                var difference = (localTime - ntpTime);
+                // Convert to type 'long' in order to remove 'micro' seconds 
+                long ms = (long)(difference.TotalMilliseconds);
+                // Convert negative values to positive values. We don't care about the '-' because we just need to know the difference between the two values
+                ms = System.Math.Abs(ms);
+                Util.writeLog("func_compareTime", "difference: " + ms.ToString(), false);
+                // Write to .dat file
+                Util.writeRPMDatFile(attributeNumber, ms.ToString());
+
+            }
+            catch (Exception exc)
+            {
+                Util.writeLog("func_compareTime", "EXCEPTION : " + exc.Message.ToString(), false);
+                Console.WriteLine(exc.Message.ToString());
+            }
+        }
+
+        static void func_SqlDbaseTime(int attributeNumber)
+        {
+            try
+            {
+                Util.writeRPMDatFile(attributeNumber, "1");
+            }
+            catch (Exception exc)
+            {
+                Util.writeLog("func_SqlDbaseTime", "EXCEPTION : " + exc.Message.ToString(), false);
+                Console.WriteLine(exc.Message.ToString());
+            }
+
+
+        }
+
+        static void func_randomNumber(int attributeNumber, int param1, int param2)
+        {
+            int startNumber = param1;
+            Util.writeLog("func_randomNumber", "startNumber = " + param1, true);
+            int endNumber = param2;
+            Util.writeLog("func_randomNumber", "endNumber = " + param2, true);
+            Random random = new Random();
+            int randomNumber = random.Next(startNumber, endNumber);
+            Util.writeLog("func_randomNumber", "randomNumber = " + randomNumber, true);
+            Util.writeRPMDatFile(attributeNumber, randomNumber.ToString());
         }
     }
+
 }
+
---
+++ rpmkit/Util.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Configuration;
+
+namespace rpmkit
+{
+    class Util
+    {
+        public static void writeRPMDatFile(int attr, string result)
+        {
+            // need to write to rpm_[attributenumber].dat
+            // contents should be
+            //    [attributenumber][char(1)][outputvalue]
+            char delimiter = (char)1;
+            // Read configuration from app settings file
+            string fileLoc = ConfigurationManager.AppSettings.Get("PathToScript");
+            writeLog("writeRPMDatFile", "Write to file '" + fileLoc + "rpm_" + attr.ToString() + ".dat' : " + result, false);
+            // Write to file
+            File.WriteAllText(fileLoc + "rpm_" + attr.ToString() + ".dat", attr.ToString() + delimiter.ToString() + result);
+        }
+
+        public static void writeLog(string funcName, string logMessage, bool isDebug)
+        {
+            string procName = Process.GetCurrentProcess().ProcessName;
+            string fileLoc = ConfigurationManager.AppSettings.Get("PathToScript");
+            string logLevel = ConfigurationManager.AppSettings.Get("LogLevel");
+            DateTime logTime = DateTime.Now;
+            string logLevelLogged;
+            logLevelLogged = (isDebug == true) ? " [DEBUG] " : " [INFO] ";
+            string convLogMessage = "\r\n" + logTime.ToString() + logLevelLogged + "Function Name: " + funcName + " - " + logMessage;
+            if (logLevel.ToUpper() == "DEBUG")
+            {
+                // Write ANY log when log level is 'DEBUG'
+                File.AppendAllText(fileLoc + "rpm_" + Program.g_AttrNum.ToString() + "_" + procName.ToString() + ".log", convLogMessage);
+            }
+            else if ((logLevel.ToUpper() == "INFO") && (isDebug == false))
+            {
+                // Write only 'INFO' logs when log level is 'INFO' 
+                File.AppendAllText(fileLoc + "rpm_" + Program.g_AttrNum.ToString() + "_" + procName.ToString() + ".log", convLogMessage);
+            }
+            else
+            // Would be the same as 
+            // if ((logLevel.ToUpper() == "INFO") && (isDebug == true))
+            {
+                //do nothing
+                return;
+            }
+        }
+    }
+
+}
